83216f2bb946b1187e76ac552d72cc2b75370b59
Calle Trogen <calleat@hotmail.com>
2024-08-16 15:44:42 +0200

    updated text

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className='text-gray-50 text-7xl font-bold'>Welcome to Chatify</h1>
-      <h3 className='m-10'>
+      <h3 className='m-10 text-xl'>
         We're excited to have you here.
         Whether you're catching up with friends, collaborating on a project, or meeting new people,
         this is the place to make your conversations smooth, fun, and secure.
