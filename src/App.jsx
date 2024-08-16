6ce936152501421c454bb5d88f667a099217e023
Calle Trogen <calleat@hotmail.com>
2024-08-16 15:28:13 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Navbar from './comp/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-gray-50 text-7xl font-bold'>Welcome to Chatify</h1>
+      <h3 className='m-10'>
+        We're excited to have you here.
+        Whether you're catching up with friends, collaborating on a project, or meeting new people,
+        this is the place to make your conversations smooth, fun, and secure.
+        Please click the button below to open the side menu.
+      </h3>
+      <Navbar />
+    </>
+    
+    
+  )
+}
+
+export default App
---
+++ src/comp/Navbar.jsx
@@ -0,0 +1,22 @@
+function Navbar() {
+    return (
+        <div className="drawer">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+                {/* Page content here */}
+                <label htmlFor="my-drawer" button class="hover:underline hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">Open Meny</label>
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                    {/* Sidebar content here */}
+                    <li><a>Home</a></li>
+                    <li><a>Log in</a></li>
+                    <li><a>Register new user</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
