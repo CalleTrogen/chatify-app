2df2a8ed2ec611fba8e1e2d3e36bb27bb3e81ac2
Calle Trogen <calleat@hotmail.com>
2024-08-27 11:37:03 +0200

    chat icon done

--- src/comp/Home.jsx
+++ src/comp/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <>
             <h1 className='text-gray-50 text-7xl font-bold'>
-                Welcome to <em>Chatify</em>
+                Welcome to <em>Chatify</em> 💬
             </h1>
 
             <h3 className='m-10 text-xl'>
