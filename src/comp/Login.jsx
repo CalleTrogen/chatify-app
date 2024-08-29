61c6cd9f4e579e4cb9f64556cfc131c7f730457d
Calle Trogen <calleat@hotmail.com>
2024-08-29 09:41:59 +0200

    const moved

--- src/comp/Login.jsx
+++ src/comp/Login.jsx
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom";
 
 
 function Login() {
+    const [jwtToken, setJwtToken] = useState(null);
+
     return (
         <div class="w-full max-w-xs">
             <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
