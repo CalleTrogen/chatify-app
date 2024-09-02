c2c32ae22c2b2faaaf4548617beebb62c77c2a42
Calle Trogen <calleat@hotmail.com>
2024-09-02 17:43:30 +0200

    beginning steps to form a chat function

--- src/comp/Chat.jsx
+++ src/comp/Chat.jsx
@@ -1,20 +1,49 @@
 import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 
-let avatars = sessionStorage.getItem("avatars");
-let token = sessionStorage.getItem("jwt");
+const AuthComponent = () => {
+    const [jwt, setJwt] = useState('');
+    const [avatarUrl, setAvatarUrl] = useState('');
+    const [isAuthed, setIsAuthed] = useState(false);
 
-function Chat() {
+    useEffect(() => {
+        // Hämtar jwt från sessionStorage
+        const savedJwt = sessionStorage.getItem('jwt');
+        if (savedJwt) {
+            setJwt(savedJwt);
+            setIsAuthed(true);
+        }
+        // Hämtar avatar från sessionStorage
+        const savedAvatarUrl = sessionStorage.getItem('avatar');
+        if (savedAvatarUrl) {
+            setAvatarUrl(savedAvatarUrl);
+        }
+    }, []);
+
+
+    return (
+        <div>
+            {isAuthed ? (
+                <div>
+                    <h1>You are authenticated</h1>
+                    {avatarUrl && <img src={avatarUrl} alt="User Avatar" />}<br />
+                    <NavLink to='/'>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">Back</button>
+                    </NavLink>
+                </div>
+            ) : (
+                <h1>Please log in</h1>
+            )}
+        </div>
+    );
+};
+
+/* function Chat() {
     return (
         <>
             <h1>This is Chat</h1><br />
-            <NavLink to='/'>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">Back</button>
-            </NavLink>
         </>
     )
-}
+} */
 
-console.log(avatars);
-console.log(token);
-
-export default Chat
+export default AuthComponent;
