3cee7342be9550a9af986e4d3e99ab1c8d6f38bc
Calle Trogen <calleat@hotmail.com>
2024-09-03 13:54:59 +0200

    display username done

--- src/comp/Chat.jsx
+++ src/comp/Chat.jsx
@@ -6,17 +6,20 @@ const AuthComponent = () => {
     const [avatarUrl, setAvatarUrl] = useState('');
     const [isAuthed, setIsAuthed] = useState(false);
     const [decodedJwt, setDecodedJwt] = useState(null);
+    const [username, setUsername] = useState('');
 
     useEffect(() => {
         // Hämtar jwt från sessionStorage
         const savedJwt = sessionStorage.getItem('jwt');
+
         if (savedJwt) {
             const decodedJwt = JSON.parse(atob(savedJwt.split('.')[1]));
-
+            sessionStorage.setItem('decodedJwt', JSON.stringify(decodedJwt));
             setDecodedJwt(decodedJwt);
             setJwt(savedJwt);
             setIsAuthed(true);
             setAvatarUrl(decodedJwt.avatar);
+            setUsername(decodedJwt.user);
         }
     }, []);
 
@@ -26,9 +29,8 @@ const AuthComponent = () => {
             {isAuthed ? (
                 <div>
                     {avatarUrl && <img src={avatarUrl} alt="User Avatar" className="object-content h-30 w-20 inline-block rounded mb-5" />}
-                    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome </h1>
+                    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome {username} </h1>
                     <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">You are authenticated. Below you can read your chat history</p>
-
                     <div>
                         <NavLink to='/'>
                             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">Back</button>
