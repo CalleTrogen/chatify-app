fa708417ce621ebb74f92e275733cd1357ab232b
Calle Trogen <calleat@hotmail.com>
2024-09-05 14:11:58 +0200

    new message get when logged in

--- src/comp/Chat.jsx
+++ src/comp/Chat.jsx
@@ -25,7 +25,7 @@ const AuthComponent = () => {
             "avatar": "https://i.pravatar.cc/150?img=3",
             "username": "Calle",
             "conversationId": null
-        }
+        },
     ]);
 
     useEffect(() => {
@@ -40,39 +40,70 @@ const AuthComponent = () => {
             setIsAuthed(true);
             setAvatarUrl(decodedJwt.avatar);
             setUsername(decodedJwt.user);
-
         }
+        fetchMessages();
     }, []);
 
-    /* const updatedChat = fakeChat.map(message => ({ //TODO: innehåller meddelande kopplat till ett specifikt username:
-        ...message,
-        avatar: decodedJwt.avatar,
-        username: decodedJwt.user
-    }));
-    setFakeChat(updatedChat); */
+
+    const [messages, setMessages] = useState([]);
+    const fetchMessages = async () => {
+
+        try {
+            const response = await fetch(`https://chatify-api.up.railway.app/messages`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + jwt
+                },
+            });
+            const data = await response.json();
+            console.log(data)
+            setMessages(data);
+        } catch (error) {
+            console.error('Error fetching messages:', error);
+        }
+    };
 
     return (
-        <div className="">
+        <div>
             {isAuthed ? (
                 <div>
                     {avatarUrl && <img src={avatarUrl} alt="User Avatar" className="object-content h-30 w-20 inline-block rounded mb-5" />}
                     <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome {username} </h1>
                     <h2 className="mb-6 text-lg font-normal text-green-500 lg:text-xl sm:px-16 xl:px-48">You are authenticated!</h2>
+
                 </div>
             ) : (
                 <h1>Couldn't be authenticated. Please try to log in again.</h1>
             )}
-            <h2 className="flex item-start space-x-4 mb-5 text-base">Message history</h2>
+            <h2 className="flex item-start space-x-4 mb-5 text-base">Message history:</h2>
             {fakeChat.map((msg, index) => (
                 <div key={index} className="flex item-start space-x-4 p-1">
                     <img src={msg.avatar} alt={`${msg.username}'s avatar`} className="w-6 h-6 rounded-full" />
                     <strong>{msg.username}:&nbsp;</strong> {msg.text}
                 </div>
             ))}
-            <NavLink to='/'>
+            <div className="space-y-4">
+                {messages.length > 0 ? (
+                    messages.map((msg, index1) => (
+                        <div key={index1} className="flex items-start space-x-4">
+                            <div className="max-w-xs p-3 rounded-lg bg-gray-100">
+                                <strong className="text-lg text-gray-500">{decodedJwt.user}:<img src={decodedJwt.avatar} className="w-8 h-8 float-right" /></strong>
+                                <p className="text-gray-700">{msg.text}</p>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <></>
+                )}
+            </div>
+            <NavLink to='/' className="p-5">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-5">Back</button>
             </NavLink>
-        </div>
+            <NavLink to="*">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-5 p-5">New message</button>
+            </NavLink>
+        </div >
     );
 };
 
